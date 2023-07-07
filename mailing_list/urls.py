403715d1b5a1d9add4da9147b388b8c4f4123410
DSulzhits <dsulzhits@gmail.com>
2023-07-07 17:13:13 +0300

    add urlpatterns for app Mailing_list

--- mailing_list/urls.py
+++ mailing_list/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from mailing_list.views import IndexView
+from mailing_list.apps import MailingListConfig
+from mailing_list.views import IndexView, ClientListView, ClientDetailView, ClientCreateView, ClientUpdateView, \
+    ClientDeleteView
+
+app_name = MailingListConfig.name
 
 urlpatterns = [
     path('', IndexView.as_view(), name='homepage'),
-    ]
+    path('clients/', ClientListView.as_view(), name='client_list'),
+    path('client/<int:pk>/', ClientDetailView.as_view(), name='client_detail'),
+    path('client/create/', ClientCreateView.as_view(), name='client_create'),
+    path('client/update/<int:pk>/', ClientUpdateView.as_view(), name='client_create'),
+    path('client/delete/<int:pk>', ClientDeleteView.as_view(), name='client_delete'),
+]
