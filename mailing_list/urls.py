a0d720bb24f4e00fa06fb301b63799fa386c8eb1
DSulzhits <dsulzhits@gmail.com>
2023-07-07 12:07:37 +0300

    Add index view, path IndexView.as_view in urlpatterns and templates: base.html; index.html; inc_catalog_head.html; inc_catalog_info.html.

--- mailing_list/urls.py
+++ mailing_list/urls.py
@@ -1,2 +1,6 @@
 from django.urls import path
+from mailing_list.views import IndexView
 
+urlpatterns = [
+    path('', IndexView.as_view(), name='homepage'),
+    ]
--- mailing_list/views.py
+++ mailing_list/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.urls import path
+from mailing_list.models import Client, MailingListMessage, MailingList, MailingListLogs
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = 'mailing_list/index.html'
+    extra_context = {
+        'title': 'Домашняя страница',
+        'object_list': MailingList.objects.all()
+    }
