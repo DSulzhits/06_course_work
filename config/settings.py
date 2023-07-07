6254b0ccb5689c9b8d70d0d56d494dede83af540
DSulzhits <dsulzhits@gmail.com>
2023-07-07 12:43:45 +0300

    Add users-settings

--- config/settings.py
+++ config/settings.py
@@ -131,10 +131,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# AUTH_USER_MODEL = 'users.User'
-# LOGIN_REDIRECT_URL = '/'
-# LOGOUT_REDIRECT_URL = '/'
-# LOGIN_URL = '/users/'
+AUTH_USER_MODEL = 'users.User'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = '/users/'
 
 CACHE_ENABLED = os.getenv('CACHE_ENABLED') == 'True'
 
