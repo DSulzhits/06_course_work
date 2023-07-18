8e76674c02b1d5f201fcb18993ab7d550979a2ac
DSulzhits <dsulzhits@gmail.com>
2023-07-18 16:44:11 +0300

    add cache settings

--- mailing_list/urls.py
+++ mailing_list/urls.py
@@ -5,26 +5,27 @@ from mailing_list.views import IndexView, ClientListView, ClientDetailView, Clie
     MailingListMessageDetailView, MailingListMessageCreateView, MailingListMessageUpdateView, \
     MailingListMessageDeleteView, MailingListListView, MailingListDetailView, MailingListCreateView, \
     MailingListUpdateView, MailingListDeleteView
+from django.views.decorators.cache import cache_page, never_cache
 
 app_name = MailingListConfig.name
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='homepage'),
+    path('', cache_page(60)(IndexView.as_view()), name='homepage'),
     path('clients/', ClientListView.as_view(), name='client_list'),
     path('clients/deactivated/', ClientDeactivatedListView.as_view(), name='client_deactivated_list'),
-    path('client/<int:pk>/', ClientDetailView.as_view(), name='client_detail'),
-    path('client/create/', ClientCreateView.as_view(), name='client_create'),
-    path('client/update/<int:pk>/', ClientUpdateView.as_view(), name='client_update'),
-    path('client/delete/<int:pk>', ClientDeleteView.as_view(), name='client_delete'),
+    path('client/<int:pk>/', cache_page(60)(ClientDetailView.as_view()), name='client_detail'),
+    path('client/create/', never_cache(ClientCreateView.as_view()), name='client_create'),
+    path('client/update/<int:pk>/', never_cache(ClientUpdateView.as_view()), name='client_update'),
+    path('client/delete/<int:pk>', never_cache(ClientDeleteView.as_view()), name='client_delete'),
     path('client/toggle/<int:pk>', client_toggle_activity, name='client_toggle_activity'),
     path('mailing_list_messages/', MailingListMessageListView.as_view(), name='message_list'),
-    path('mailing_list_message/<int:pk>/', MailingListMessageDetailView.as_view(), name='message_detail'),
-    path('mailing_list_message/create/', MailingListMessageCreateView.as_view(), name='message_create'),
-    path('mailing_list_message/update/<int:pk>/', MailingListMessageUpdateView.as_view(), name='message_update'),
-    path('mailing_list_message/delete/<int:pk>/', MailingListMessageDeleteView.as_view(), name='message_delete'),
+    path('mailing_list_message/<int:pk>/', cache_page(60)(MailingListMessageDetailView.as_view()), name='message_detail'),
+    path('mailing_list_message/create/', never_cache(MailingListMessageCreateView.as_view()), name='message_create'),
+    path('mailing_list_message/update/<int:pk>/', never_cache(MailingListMessageUpdateView.as_view()), name='message_update'),
+    path('mailing_list_message/delete/<int:pk>/', never_cache(MailingListMessageDeleteView.as_view()), name='message_delete'),
     path('mailing_lists/', MailingListListView.as_view(), name='mailing_lists_list'),
-    path('mailing_lists/<int:pk>/', MailingListDetailView.as_view(), name='mailing_list_detail'),
-    path('mailing_lists/create/', MailingListCreateView.as_view(), name='mailing_list_create'),
-    path('mailing_lists/update/<int:pk>/', MailingListUpdateView.as_view(), name='mailing_list_update'),
-    path('mailing_lists/delete/<int:pk>/', MailingListDeleteView.as_view(), name='mailing_list_delete'),
+    path('mailing_lists/<int:pk>/', cache_page(60)(MailingListDetailView.as_view()), name='mailing_list_detail'),
+    path('mailing_lists/create/', never_cache(MailingListCreateView.as_view()), name='mailing_list_create'),
+    path('mailing_lists/update/<int:pk>/', never_cache(MailingListUpdateView.as_view()), name='mailing_list_update'),
+    path('mailing_lists/delete/<int:pk>/', never_cache(MailingListDeleteView.as_view()), name='mailing_list_delete'),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import never_cache
 
 from users.apps import UsersConfig
 from users.views import LoginView, LogoutView, RegisterView, UserUpdateView, generate_new_password
@@ -8,7 +9,7 @@ app_name = UsersConfig.name
 urlpatterns = [
     path('', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('profile/', UserUpdateView.as_view(), name='profile'),
-    path('profile/genpassword/', generate_new_password, name='generate_new_password'),
+    path('register/', never_cache(RegisterView.as_view()), name='register'),
+    path('profile/', never_cache(UserUpdateView.as_view()), name='profile'),
+    path('profile/genpassword/', never_cache(generate_new_password), name='generate_new_password'),
 ]
